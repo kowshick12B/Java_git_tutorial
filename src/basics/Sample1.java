937c3f339d8e95f0806f2154ed9fae2f958c1224
kowshick12B <kowshick12@gmail.com>
2024-12-12 11:32:15 +0530

    second-commint

--- src/basics/Sample1.java
+++ src/basics/Sample1.java
@@ -4,6 +4,9 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 	 System.out.println("Git_tutorial");
+	 System.out.println("Github tutorial");
+	 System.out.println("Java");
+	 System.out.println("Selenium");
 
 	}
 
