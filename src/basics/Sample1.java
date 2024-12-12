064f1104ea010fc6de0c1ff86a7af8ad96f9c76f
kowshick12B <kowshick12@gmail.com>
2024-12-12 11:34:53 +0530

    Update Sample1 by kowshick.java

--- src/basics/Sample1.java
+++ src/basics/Sample1.java
@@ -2,12 +2,17 @@ package basics;
 
 public class Sample1 {
 
-	public static void main(String[] args) {
-	 System.out.println("Git_tutorial");
-	 System.out.println("Github tutorial");
-	 System.out.println("Java");
-	 System.out.println("Selenium");
+	public static void m1()
+	{
+	  System.out.println("Git_tutorial");
+	  System.out.println("Github tutorial");
+	  System.out.println("Java");
+	  System.out.println("Selenium");
+	}
 
+	public static void main(String[] args) {
+	 
+	  m1();
 	}
 
 }
